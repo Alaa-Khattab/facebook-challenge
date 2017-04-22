9434ee45fe24a3463913560b8c894c13ee01d9ce
4daliaomry4 <alaakhattab92@gmail.com>
2017-04-22 13:25:12 +0300

    change function

--- src/receiveMessage.js
+++ src/receiveMessage.js
@@ -1,20 +1,23 @@
- const reply=require('./sendReply.js');
-const message=require('./sendTextMessage.js');
+ const reply = require('./sendReply.js');
+ const message = require('./sendTextMessage.js');
 
-function receivedMessage(event){
+ function receivedMessage(event) {
    var senderID = event.sender.id;
-    var message = event.message;
-     var messageText = message.text;
-     if(messageText){
+   var message = event.message;
+   var messageAttachments = message.attachments;
+   var messageText = message.text;
+   console.log('message at trcivedMSG', message);
+   if (messageText) {
      switch (messageText) {
-      case 'start':
-        reply.sendReply(senderID);
-        break;
-      default:
-        message.sendTextMessage(senderID,'انتا قاعد بتتخوث, اكتب زي الخلق وانهي...');
-    }
-  } else if (messageAttachments) {
-    message.sendTextMessage(senderID, 'Message with attachment received');
-  }
-}
-module.exports={receivedMessage:receivedMessage}
+       case 'start':
+         reply.sendReply(senderID);
+       default:
+         message.sendTextMessage(senderID, 'انتا قاعد بتتخوث, اكتب زي الخلق وانهي...');
+     }
+   } else if (messageAttachments) {
+     message.sendTextMessage(senderID, 'Message with attachment received');
+   }
+ }
+ module.exports = {
+   receivedMessage: receivedMessage
+ }
